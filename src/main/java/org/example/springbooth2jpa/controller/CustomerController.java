970269becaebf785f87655e242cded1bc722a3a1
cjy <jinyoungchoi4@gmail.com>
2024-05-08 23:51:02 +0900

    [연습]

--- src/main/java/org/example/springbooth2jpa/controller/CustomerController.java
+++ src/main/java/org/example/springbooth2jpa/controller/CustomerController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/customers")
-public class CustomerController {
+public class CustomerController {   
 
     private final CustomerService customerService;//////
 
